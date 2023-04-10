07d93bcba0993331d0171e9ee44ffebaedd0e53a
Gabriele Bologna <bolognagabriele2003@gmail.com>
2023-04-10 23:00:23 +0200

    First commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,154 @@
+﻿partial class MainForm
+{
+    private System.ComponentModel.IContainer components = null;
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    private void InitializeComponent()
+    {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2ControlBox2 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.Animated = true;
+            this.guna2ControlBox1.ControlBoxStyle = Guna.UI2.WinForms.Enums.ControlBoxStyle.Custom;
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.guna2ControlBox1.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(451, 12);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox1.TabIndex = 0;
+            // 
+            // guna2ControlBox2
+            // 
+            this.guna2ControlBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox2.Animated = true;
+            this.guna2ControlBox2.ControlBoxStyle = Guna.UI2.WinForms.Enums.ControlBoxStyle.Custom;
+            this.guna2ControlBox2.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox;
+            this.guna2ControlBox2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.guna2ControlBox2.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.guna2ControlBox2.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox2.Location = new System.Drawing.Point(400, 12);
+            this.guna2ControlBox2.Name = "guna2ControlBox2";
+            this.guna2ControlBox2.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox2.TabIndex = 1;
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.Animated = true;
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.guna2Button1.Location = new System.Drawing.Point(130, 112);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(218, 45);
+            this.guna2Button1.TabIndex = 2;
+            this.guna2Button1.Text = "Process media file";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2TextBox1
+            // 
+            this.guna2TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.guna2TextBox1.DefaultText = "";
+            this.guna2TextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.guna2TextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.guna2TextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.guna2TextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2TextBox1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.guna2TextBox1.Location = new System.Drawing.Point(30, 64);
+            this.guna2TextBox1.Name = "guna2TextBox1";
+            this.guna2TextBox1.PasswordChar = '\0';
+            this.guna2TextBox1.PlaceholderText = "";
+            this.guna2TextBox1.SelectedText = "";
+            this.guna2TextBox1.Size = new System.Drawing.Size(391, 36);
+            this.guna2TextBox1.TabIndex = 3;
+            this.guna2TextBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.guna2TextBox1_DragDrop);
+            this.guna2TextBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.guna2TextBox1_DragEnter);
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.Animated = true;
+            this.guna2Button2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.guna2Button2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button2.ForeColor = System.Drawing.Color.White;
+            this.guna2Button2.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.guna2Button2.Location = new System.Drawing.Point(431, 64);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.Size = new System.Drawing.Size(47, 36);
+            this.guna2Button2.TabIndex = 4;
+            this.guna2Button2.Text = "...";
+            this.guna2Button2.Click += new System.EventHandler(this.guna2Button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "All files (*.*)|*.*";
+            this.openFileDialog1.Title = "Open your media file...";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Title = "Save your processed media file...";
+            // 
+            // MainForm
+            // 
+            this.AllowResize = false;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(507, 187);
+            this.Controls.Add(this.guna2Button2);
+            this.Controls.Add(this.guna2TextBox1);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.guna2ControlBox2);
+            this.Controls.Add(this.guna2ControlBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.State = MetroSuite.MetroForm.FormState.Normal;
+            this.Style = MetroSuite.Design.Style.Dark;
+            this.Text = "Blur Faces AI";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.ResumeLayout(false);
+
+    }
+
+    private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+    private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox2;
+    private Guna.UI2.WinForms.Guna2Button guna2Button1;
+    private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+    private Guna.UI2.WinForms.Guna2Button guna2Button2;
+    private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+}
---
+++ MainForm.cs
@@ -0,0 +1,227 @@
+﻿using MetroSuite;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Dnn;
+using System.Diagnostics;
+using Microsoft.WindowsAPICodePack.Shell;
+using System.Threading;
+using System.Runtime;
+using System;
+using System.Runtime.InteropServices;
+
+public partial class MainForm : MetroForm
+{
+    private Net faceNet = CvDnn.ReadNetFromCaffe("models\\deploy.prototxt",
+        "models\\res10_300x300_ssd_iter_140000_fp16.caffemodel");
+
+    [DllImport("psapi.dll")]
+    static extern int EmptyWorkingSet(IntPtr hwProc);
+
+    [DllImport("kernel32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool SetProcessWorkingSetSize(IntPtr process, UIntPtr minimumWorkingSetSize, UIntPtr maximumWorkingSetSize);
+
+    public MainForm()
+    {
+        InitializeComponent();
+        ProcessFramesFolder();
+        Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.RealTime;
+
+        Thread clearRamThread = new Thread(ClearRAM);
+        clearRamThread.Priority = ThreadPriority.Highest;
+        clearRamThread.Start();
+    }
+
+    private void guna2Button2_Click(object sender, System.EventArgs e)
+    {
+        if (openFileDialog1.ShowDialog().Equals(DialogResult.OK))
+        {
+            guna2TextBox1.Text = openFileDialog1.FileName;
+        }
+    }
+
+    private void guna2Button1_Click(object sender, System.EventArgs e)
+    {
+        if (!System.IO.File.Exists(guna2TextBox1.Text))
+        {
+            MessageBox.Show("The specified file does not exist.", Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+
+        if (!System.Web.MimeMapping.GetMimeMapping(guna2TextBox1.Text).StartsWith("image/") &&
+            !System.Web.MimeMapping.GetMimeMapping(guna2TextBox1.Text).StartsWith("video/") &&
+            !System.Web.MimeMapping.GetMimeMapping(guna2TextBox1.Text).StartsWith("application/octet-stream"))
+        {
+            MessageBox.Show(System.Web.MimeMapping.GetMimeMapping(guna2TextBox1.Text));
+            MessageBox.Show("The specified file is not a image or a video.", Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+
+        string extension = System.IO.Path.GetExtension(guna2TextBox1.Text).ToLower().Substring(1);
+        saveFileDialog1.Filter = $"{extension.ToUpper()} file (*.{extension})|*.{extension}";
+
+        if (!saveFileDialog1.ShowDialog().Equals(DialogResult.OK))
+        {
+            return;
+        }
+
+        if (System.IO.File.Exists(saveFileDialog1.FileName))
+        {
+            System.IO.File.Delete(saveFileDialog1.FileName);
+        }
+
+        guna2TextBox1.Enabled = false;
+        guna2Button2.Enabled = false;
+
+        if (System.Web.MimeMapping.GetMimeMapping(guna2TextBox1.Text).StartsWith("image/"))
+        {
+            BlurFaceInImage(guna2TextBox1.Text, saveFileDialog1.FileName);
+        }
+        else
+        {
+            BlurFaceInVideo(guna2TextBox1.Text, saveFileDialog1.FileName);
+        }
+
+        guna2TextBox1.Enabled = true;
+        guna2Button2.Enabled = true;
+
+        MessageBox.Show("Processing finished succesfully. Enjoy!", Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+    }
+
+    public void BlurFaceInImage(string inputImage, string outputImage)
+    {
+        try
+        {
+            Mat image = Cv2.ImRead(inputImage);
+
+            int frameHeight = image.Rows;
+            int frameWidth = image.Cols;
+
+            Mat blob = CvDnn.BlobFromImage(image, 1.0, new Size(300, 300), new Scalar(104, 117, 123), false, false);
+            faceNet.SetInput(blob, "data");
+
+            Mat detection = faceNet.Forward("detection_out");
+            Mat detectionMat = new Mat(detection.Size(2), detection.Size(3), MatType.CV_32F, detection.Ptr(0));
+
+            for (int i = 0; i < detectionMat.Rows; i++)
+            {
+                float confidence = detectionMat.At<float>(i, 2);
+
+                if (confidence > 0.5)
+                {
+                    int x1 = (int)(detectionMat.At<float>(i, 3) * frameWidth);
+                    int y1 = (int)(detectionMat.At<float>(i, 4) * frameHeight);
+                    int x2 = (int)(detectionMat.At<float>(i, 5) * frameWidth);
+                    int y2 = (int)(detectionMat.At<float>(i, 6) * frameHeight);
+
+                    Mat faceImg = new Mat(image, new OpenCvSharp.Range(y1, y2), new OpenCvSharp.Range(x1, x2));
+
+                    Mat faceBlur = new Mat();
+                    Cv2.GaussianBlur(faceImg, faceBlur, new Size(67, 67), 90, 60);
+                    image[new OpenCvSharp.Range(y1, y2), new OpenCvSharp.Range(x1, x2)] = faceBlur;
+                    image.SaveImage(outputImage);
+                    faceBlur.Dispose();
+                    faceImg.Dispose();
+                }
+            }
+
+            detection.Dispose();
+            detectionMat.Dispose();
+            blob.Dispose();
+            image.Dispose();
+        }
+        catch
+        {
+
+        }
+    }
+
+    public void BlurFaceInVideo(string inputVideo, string outputVideo)
+    {
+        string extension = System.IO.Path.GetExtension(outputVideo).ToLower().Substring(1);
+
+        ProcessFramesFolder();
+        ShellFile shellFile = ShellFile.FromFilePath(inputVideo);
+        string FPS = (shellFile.Properties.System.Video.FrameRate.Value / 1000).ToString();
+        RunFFMpeg($"-threads {Environment.ProcessorCount} -i \"{inputVideo}\" -vn -acodec copy \"{System.IO.Path.GetFullPath("frames")}\\audio.aac\"");
+        RunFFMpeg($"-threads {Environment.ProcessorCount} -i \"{inputVideo}\" -vf fps={FPS} \"{System.IO.Path.GetFullPath("frames")}\\%16d.png\"");
+
+        foreach (string file in System.IO.Directory.GetFiles("frames"))
+        {
+            BlurFaceInImage(file, file);
+        }
+
+        RunFFMpeg($"-threads {Environment.ProcessorCount} -r {FPS} -i \"{System.IO.Path.GetFullPath("frames")}\\%16d.png\" -b:v {FPS}M \"{outputVideo}\"");
+        RunFFMpeg($"-threads {Environment.ProcessorCount} -i \"{outputVideo}\" -i \"{System.IO.Path.GetFullPath("frames")}\\audio.aac\" -c copy -map 0:v:0 -map 1:a:0 \"{System.IO.Path.GetFullPath("models")}\\newOutput.{extension}\"");
+
+        while (!System.IO.File.Exists(outputVideo) || !System.IO.File.Exists($"models\\newOutput.{extension}"))
+        {
+            Thread.Sleep(1);
+        }
+
+        System.IO.File.Delete(outputVideo);
+        System.IO.File.Move($"models\\newOutput.{extension}", outputVideo);
+
+        ProcessFramesFolder();
+    }
+
+    public void ProcessFramesFolder()
+    {
+        if (!System.IO.Directory.Exists("frames"))
+        {
+            System.IO.Directory.CreateDirectory("frames");
+        }
+
+        foreach (string file in System.IO.Directory.GetFiles("frames"))
+        {
+            System.IO.File.Delete(file);
+        }
+    }
+
+    public void RunFFMpeg(string arguments)
+    {
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = "ffmpeg.exe",
+            Arguments = arguments,
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            CreateNoWindow = true,
+            WindowStyle = ProcessWindowStyle.Hidden
+        }).WaitForExit();
+    }
+
+    public void ClearRAM()
+    {
+        while (true)
+        {
+            Thread.Sleep(3000);
+            EmptyWorkingSet(Process.GetCurrentProcess().Handle);
+            GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
+            GC.Collect(GC.MaxGeneration);
+            GC.WaitForPendingFinalizers();
+            SetProcessWorkingSetSize(Process.GetCurrentProcess().Handle, (UIntPtr)0xFFFFFFFF, (UIntPtr)0xFFFFFFFF);
+        }
+    }
+
+    private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+    {
+        Process.GetCurrentProcess().Kill();
+    }
+
+    private void guna2TextBox1_DragEnter(object sender, DragEventArgs e)
+    {
+        if (e.Data.GetDataPresent(DataFormats.FileDrop))
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+    }
+
+    private void guna2TextBox1_DragDrop(object sender, DragEventArgs e)
+    {
+        if (e.Data.GetDataPresent(DataFormats.FileDrop))
+        {
+            guna2TextBox1.Text = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+        }
+    }
+}
